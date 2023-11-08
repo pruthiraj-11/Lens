7b35ac6dabd242e1635e19e05a62b3b395cca68c
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-11-08 14:45:56 +0530

    Building

--- app/src/main/java/com/app/lens/Fragments/LanguageTranslationFragment.java
+++ app/src/main/java/com/app/lens/Fragments/LanguageTranslationFragment.java
@@ -1,25 +1,80 @@
 package com.app.lens.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-import com.app.lens.R;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.app.lens.databinding.FragmentLanguageTranslationBinding;
+import com.google.mlkit.nl.translate.TranslateLanguage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LanguageTranslationFragment extends Fragment {
     FragmentLanguageTranslationBinding binding;
+//    String[] fromLanguages={"From","English","Afrikaans","Arabic","Belarusian","Bulgarian","Bengali","Catalan","Czech","Welsh","Hindi","Urdu"};
+//    String[] toLanguages={"To","English","Afrikaans","Arabic","Belarusian","Bulgarian","Bengali","Catalan","Czech","Welsh","Hindi","Urdu"};
+    private final static int REQUEST_CODE=74;
+    int languageCode,toLanguageCode,fromLanguageCode=0;
+    List<String> languageList=new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+
     public LanguageTranslationFragment() {
+        languageList=TranslateLanguage.getAllLanguages();
+        arrayAdapter=new ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item,languageList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.sourcespinner.setAdapter(arrayAdapter);
+        binding.destspinner.setAdapter(arrayAdapter);
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding=FragmentLanguageTranslationBinding.inflate(inflater);
+//        String[] languageListFrom = new String[languageList.size()];
+//        for(int i=0;i<languageList.size();i++){
+//            languageListFrom[i]= languageList.get(i);
+//        }
+        binding.sourcespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                fromLanguageCode=getLangauge(fromLanguages[i]);
+                Toast.makeText(getContext(), languageList.get(i), Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
+        binding.destspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                fromLanguageCode=getLangauge(fromLanguages[i]);
+                Toast.makeText(getContext(), languageList.get(i), Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        binding.translatebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return binding.getRoot();
     }
+//    public int getLangauge(String language){
+//        int langaugecode=0;
+//        switch (language){
+//            case "English":
+//        }
+//    }
 }
