85a4b8705e360a0edaa5736afe0ae735225a5784
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-11-12 18:48:39 +0530

    Building

--- app/src/main/java/com/app/lens/Fragments/TextRecognitionFragment.java
+++ app/src/main/java/com/app/lens/Fragments/TextRecognitionFragment.java
@@ -1,10 +1,13 @@
 package com.app.lens.Fragments;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
@@ -63,8 +66,15 @@ public class TextRecognitionFragment extends Fragment implements ImageAnalysis.A
 
         ActivityResultLauncher<String> activityResultLauncher=registerForActivityResult(new ActivityResultContracts.GetContent(), o -> {
             if(o!=null){
+                Bitmap bitmap = null;
+                ContentResolver contentResolver = requireContext().getContentResolver();
                 try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(), o);
+                    if(Build.VERSION.SDK_INT < 28) {
+                        bitmap = MediaStore.Images.Media.getBitmap(contentResolver, o);
+                    } else {
+                        ImageDecoder.Source source = ImageDecoder.createSource(contentResolver, o);
+                        bitmap = ImageDecoder.decodeBitmap(source);
+                    }
                     InputImage inputImage=InputImage.fromBitmap(bitmap,0);
                     detectText(inputImage);
                 } catch (IOException e) {
